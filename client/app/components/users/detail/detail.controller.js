19ec27aee1ad71b2e54f1e1e2d40837afe8d785c
Mucktar Pakpahan <mucktar@bridestory.com>
2016-08-20 13:59:18 +0700

    fix user change password

--- client/app/components/users/detail/detail.controller.js
+++ client/app/components/users/detail/detail.controller.js
@@ -1,31 +1,43 @@
 class DetailController {
-  constructor($http, User, toastr, $state) {
+  constructor($http, User, toastr, $state, apiURL) {
     "ngInject";
 
     this.$http = $http;
     this.User = User;
     this.$state = $state;
+    this.apiURL = apiURL;
+    this.toastr = toastr;
   }
 
-  changPassword = (form, $event) => {
+  changePassword = (form, $event) => {
     const request = {
       method: 'PUT',
-      url: apiURL + '/user',
-      data: this.userDetail,
+      url: this.apiURL + '/user/' + this.userDetail.id,
+      data: {
+        email: this.userDetail.email,
+        password:this.userDetail.password
+      },
       headers: {
         'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
         'Authorization': 'Bearer' + ' ' + this.User.getAuth().access_token
       }
     };
 
-    this.$http(request)
-      .success((res) => {
-        this.toastr.success('Password berhasil di ganti', 'Success');
-        this.$state.go('users');
-      })
-      .error((error) => {
-        this.toastr.error('Isi semua data dengan benar', 'Error');
-      });
+    this.formSubmitted = true;
+
+    if (form.$valid) {
+      this.$http(request)
+        .success((res) => {
+          this.toastr.success('Password berhasil di ganti', 'Success');
+          this.$state.go('users');
+        })
+        .error((error) => {
+          form.password.$setValidity("required", false);
+          this.toastr.error('Isi semua data dengan benar', 'Error');
+        });
+    } else {
+      this.toastr.error('Isi semua data dengan benar', 'Validation Error');
+    }
 
     $event.preventDefault();
   }

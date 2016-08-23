03454f995e654b2094197ff5a1e06cab1ac3fed8
Mucktar Pakpahan <mucktar@bridestory.com>
2016-08-24 01:01:55 +0700

    update dashboard

--- client/app/components/dashboard/belanja/belanja.controller.js
+++ client/app/components/dashboard/belanja/belanja.controller.js
@@ -3,6 +3,7 @@ class BelanjaController {
     this.gambar3 = {
       options: {
         chart: {
+          height: 300,
           type: 'pie'
         }
       },
@@ -50,6 +51,7 @@ class BelanjaController {
     this.gambar5 = {
       options: {
         chart: {
+          height: 300,
           type: 'pie'
         }
       },
@@ -96,6 +98,78 @@ class BelanjaController {
         }]
       }]
     };
+
+
+    this.gambar6 = {
+      options: {
+        chart: {
+          height: 615,
+          type: 'bar'
+        },
+        plotOptions: {
+          series: {
+            stacking: 'normal'
+          }
+        },
+      },
+      title: {
+        text: 'Jenis Pengeluaran Belanja Litbang Menurut Lembaga'
+      },
+      xAxis: {
+        categories: [
+          'Kementrian Pertanian',
+          'BPPT',
+          'Kementrian Perindutrian',
+          'LIPI',
+          'Balitbangda',
+          'Kementrian Kebudayaan Dan Pariwisata',
+          'Kementrian Kehutanan',
+          'BATAN',
+          'Kementrian Pekerjaan Umum',
+          'Kementrian Kelautan dan Perikanan',
+          'Kementrian Kesehatan',
+          'Bappeda',
+          'Kementrian Komunikasi dan Informatika',
+          'Kementrian Perhubungan',
+          'Kementrian Pertahanan',
+          'Kementrian ESDM',
+          'Kementrian Agama',
+          'Kementrian Sosial',
+          'Kementrian Tenaga Kerja dan Transmigrasi',
+          'Kementrian Hukum dan HAM',
+          'Kementrian Koperasi dan UKM',
+          'Kementrian Luar Negeri',
+          'Kementrian Pendidikan Nasional',
+          'Kementrian Perdagangan',
+          'BSN',
+          'Bakosurtanal',
+          'LAPAN'
+        ]
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'Total fruit consumption'
+        }
+      },
+      legend: {
+        reversed: true
+      },
+      series: [{
+          name: 'Tanah, Gedung dan Bangunan',
+          data: [5,3,4,3,4,6,7,4,2,6,7,8,9,4,4,5,7,2,9,4,7,3,8,1,4,9,9]
+      }, {
+          name: 'Kendaraan, Mesin dan Peralatan',
+          data: [2,2,3,5,6,7,8,5,3,2,3,5,7,2,1,3,8,8,9,5,3,5,2,5,3,2,2]
+      }, {
+          name: 'Belanja Pegawai',
+          data: [3, 4, 4]
+      },{
+          name: 'Belanja Operasional',
+          data: [3, 4, 4]
+      }]
+    };
+
 
     this.gambar7 = {
       options: {
@@ -157,53 +231,53 @@ class BelanjaController {
       }]
     };
 
-    this.gambar8 = {
-      options: {
-        chart: {
-          type: 'bar',
-          stacking: 'normal'
-        },
-        plotOptions: {
-          series: {
-            stacking: 'normal'
-          }
-        },
-      },
-      title: {
-        text: 'Komposisi Sumber Pendanaan Litbang'
-      },
-      xAxis: {
-        categories: ['LPNK', 'LPK', 'Balitbanda/Bappeda']
-      },
-      yAxis: {
-        min: 0,
-        title: {
-          text: 'Total fruit consumption'
-        }
-      },
-      legend: {
-        reversed: true
-      },
-      series: [{
-          name: 'DIPA Pemerintah',
-          data: [5, 3, 4]
-      }, {
-          name: 'Non-DIPA Instansi Pemerintah',
-          data: [2, 2, 3]
-      }, {
-          name: 'Non-DIPA Luar Negeri',
-          data: [3, 4, 4]
-      },{
-          name: 'Non-DIPA Swasta',
-          data: [3, 4, 4]
-      }, {
-          name: 'DIPA PHLN',
-          data: [3, 4, 4]
-      }, {
-          name: 'DIPA PNPB',
-          data: [3, 4, 4]
-      }]
-    };
+    // this.gambar8 = {
+    //   options: {
+    //     chart: {
+    //       type: 'bar',
+    //       stacking: 'normal'
+    //     },
+    //     plotOptions: {
+    //       series: {
+    //         stacking: 'normal'
+    //       }
+    //     },
+    //   },
+    //   title: {
+    //     text: 'Komposisi Sumber Pendanaan Litbang'
+    //   },
+    //   xAxis: {
+    //     categories: ['LPNK', 'LPK', 'Balitbanda/Bappeda']
+    //   },
+    //   yAxis: {
+    //     min: 0,
+    //     title: {
+    //       text: 'Total fruit consumption'
+    //     }
+    //   },
+    //   legend: {
+    //     reversed: true
+    //   },
+    //   series: [{
+    //       name: 'DIPA Pemerintah',
+    //       data: [5, 3, 4]
+    //   }, {
+    //       name: 'Non-DIPA Instansi Pemerintah',
+    //       data: [2, 2, 3]
+    //   }, {
+    //       name: 'Non-DIPA Luar Negeri',
+    //       data: [3, 4, 4]
+    //   },{
+    //       name: 'Non-DIPA Swasta',
+    //       data: [3, 4, 4]
+    //   }, {
+    //       name: 'DIPA PHLN',
+    //       data: [3, 4, 4]
+    //   }, {
+    //       name: 'DIPA PNPB',
+    //       data: [3, 4, 4]
+    //   }]
+    // };
 
     this.gambar9 = {
       options: {
@@ -249,6 +323,77 @@ class BelanjaController {
           name: 'Penelitian Dasar',
           y: 17.66
         }]
+      }]
+    };
+
+
+    this.gambar10 = {
+      options: {
+        chart: {
+          height: 600,
+          type: 'bar'
+        },
+        plotOptions: {
+          series: {
+            stacking: 'normal'
+          }
+        },
+      },
+      title: {
+        text: 'Distribusi Biaya Litbang menurut Jenis Penelitian per Lembaga Litbang'
+      },
+      xAxis: {
+        categories: [
+          'Kementrian Pertanian',
+          'BPPT',
+          'Kementrian Perindutrian',
+          'LIPI',
+          'Balitbangda',
+          'Kementrian Kebudayaan Dan Pariwisata',
+          'Kementrian Kehutanan',
+          'BATAN',
+          'Kementrian Pekerjaan Umum',
+          'Kementrian Kelautan dan Perikanan',
+          'Kementrian Kesehatan',
+          'Bappeda',
+          'Kementrian Komunikasi dan Informatika',
+          'Kementrian Perhubungan',
+          'Kementrian Pertahanan',
+          'Kementrian ESDM',
+          'Kementrian Agama',
+          'Kementrian Sosial',
+          'Kementrian Tenaga Kerja dan Transmigrasi',
+          'Kementrian Hukum dan HAM',
+          'Kementrian Koperasi dan UKM',
+          'Kementrian Luar Negeri',
+          'Kementrian Pendidikan Nasional',
+          'Kementrian Perdagangan',
+          'BSN',
+          'Bakosurtanal',
+          'LAPAN'
+        ]
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'Total fruit consumption'
+        }
+      },
+      legend: {
+        reversed: true
+      },
+      series: [{
+          name: 'Tanah, Gedung dan Bangunan',
+          data: [5,3,4,3,4,6,7,4,2,6,7,8,9,4,4,5,7,2,9,4,7,3,8,1,4,9,9]
+      }, {
+          name: 'Kendaraan, Mesin dan Peralatan',
+          data: [2,2,3,5,6,7,8,5,3,2,3,5,7,2,1,3,8,8,9,5,3,5,2,5,3,2,2]
+      }, {
+          name: 'Belanja Pegawai',
+          data: [3, 4, 4]
+      },{
+          name: 'Belanja Operasional',
+          data: [3, 4, 4]
       }]
     };
 
@@ -307,6 +452,8 @@ class BelanjaController {
         }]
       }]
     };
+
+
   }
 }
 
--- client/app/components/dashboard/dashboard.controller.js
+++ client/app/components/dashboard/dashboard.controller.js
@@ -1,6 +1,6 @@
 class DashboardController {
   constructor() {
-    this.chartConfig = {
+    this.sebaranRespondenConfig = {
       options: {
         chart: {
           height: 500,
@@ -57,6 +57,67 @@ class DashboardController {
       }],
       title: {
         text: 'Sebaran Responden Menurut Lembaga'
+      },
+      loading: false
+    };
+
+    this.anggaranLitbangConfig = {
+      options: {
+        chart: {
+          height: 500,
+          type: 'bar'
+        }
+      },
+      xAxis: {
+        categories: [
+          'Kementrian Pertanian',
+          'BPPT',
+          'Kementrian Perindutrian',
+          'LIPI',
+          'Balitbangda',
+          'Kementrian Kebudayaan Dan Pariwisata',
+          'Kementrian Kehutanan',
+          'BATAN',
+          'Kementrian Pekerjaan Umum',
+          'Kementrian Kelautan dan Perikanan',
+          'Kementrian Kesehatan',
+          'Bappeda',
+          'Kementrian Komunikasi dan Informatika',
+          'Kementrian Perhubungan',
+          'Kementrian Pertahanan',
+          'Kementrian ESDM',
+          'Kementrian Agama',
+          'Kementrian Sosial',
+          'Kementrian Tenaga Kerja dan Transmigrasi',
+          'Kementrian Hukum dan HAM',
+          'Kementrian Koperasi dan UKM',
+          'Kementrian Luar Negeri',
+          'Kementrian Pendidikan Nasional',
+          'Kementrian Perdagangan',
+          'BSN',
+          'Bakosurtanal',
+          'LAPAN'
+        ],
+        title: {
+            text: null
+        }
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'Milyar Rupiah',
+          align: 'high'
+        },
+        labels: {
+          overflow: 'justify'
+        }
+      },
+      series: [{
+        name: 'Jumlah Anggaran',
+        data: [28, 59, 22, 17, 15, 23 ,13 ,11, 18, 9, 8, 8, 7, 5, 4, 14, 3, 3, 12, 2, 2, 11, 1, 6, 1, 1, 1]
+      }],
+      title: {
+        text: 'Total Anggaran Litbang Sektor Pemerintah'
       },
       loading: false
     };

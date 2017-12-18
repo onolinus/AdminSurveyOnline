ee98454994d8f505f241e66e4755da8024ca7a1a
mucktar <mucktar.papahan@gmail.com>
2017-12-18 09:00:14 +0700

    change env

--- app.config.js
+++ app.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  api_url:  'http://api.esurvei.ristekdikti.dev', //'http://api.survey.local',
+  api_url:  'http://api.esurvei.ristekdikti.go.id', //'http://api.survey.local',
   client_id: 2,
   client_secret: 'GKcKleCLtT6U3UvKJfKzQoz8elSfGHDGVIOZmY2J',
 };

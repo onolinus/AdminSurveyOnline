aae2e816487c4d60bc03b2234a468e3a721e675b
mucktar <mucktar.papahan@gmail.com>
2018-01-22 08:46:14 +0700

    dev build

--- app.config.js
+++ app.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  api_url:  'http://api.esurvei.ristekdikti.go.id', //'http://api.survey.local',
+  api_url:  'http://api.esurvei.ristekdikti.test', //'http://api.survey.local',
   client_id: 2,
   client_secret: 'GKcKleCLtT6U3UvKJfKzQoz8elSfGHDGVIOZmY2J',
+  static_token: 'CIbfnu3CBZuP3Oss6K6YCoeV5RVGl25ion4yHQyyPf4IGXiTR3',
 };
